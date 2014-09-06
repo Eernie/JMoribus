9b562807b25242a8158eeaa4a1a14810a15aeb7b
Robert Kuipers <robert.kuipers@gmail.com>
2014-09-06 15:41:48 +0200

    Add support for context providers and provide a default implementation

--- src/main/java/nl/eernie/jmoribus/configuration/Configuration.java
+++ src/main/java/nl/eernie/jmoribus/configuration/Configuration.java
@@ -1,6 +1,7 @@
 package nl.eernie.jmoribus.configuration;
 
 
+import nl.eernie.jmoribus.context.ContextProvider;
 import nl.eernie.jmoribus.reporter.ConcurrentReporter;
 
 import java.util.List;
@@ -12,4 +13,6 @@ public interface Configuration {
     List<Object> getSteps(Context context);
 
     void addSteps(List<Object> steps);
+
+    ContextProvider getContextProvider();
 }
---
+++ src/main/java/nl/eernie/jmoribus/context/ContextProvider.java
@@ -0,0 +1,10 @@
+package nl.eernie.jmoribus.context;
+
+public interface ContextProvider {
+
+    String get(String variableName);
+
+    void set(String variableName, String value);
+
+    boolean isVariableSet(String variableName);
+}
