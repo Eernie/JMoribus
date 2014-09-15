b59fed60bd7d80a6b0878d888004cea0c9b1f357
Erwin Oldenkamp <oldenkamperwin@gmail.com>
2014-09-15 19:44:50 +0200

    Revert make reflectionParser package protected

--- src/main/java/nl/eernie/jmoribus/parser/ReflectionParser.java
+++ src/main/java/nl/eernie/jmoribus/parser/ReflectionParser.java
@@ -11,7 +11,7 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
-final class ReflectionParser {
+public final class ReflectionParser {
 
     private ReflectionParser() {
     }
